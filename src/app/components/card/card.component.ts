206df28d31fb61e2c0a32adb46a36d8d5b0729a7
DanielSharp01 <DanielSharp01@gmail.com>
2021-03-17 12:58:36 +0100

    Almost ready game view

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  public image: 'angular' | 'd3' | 'jenkins' | 'postcss' | 'react' | 'redux' | 'sass' | 'splendex' | 'ts' | 'webpack';
+  @Input() public image: 'angular' | 'd3' | 'jenkins' | 'postcss' | 'react' | 'redux' | 'sass' | 'splendex' | 'ts' | 'webpack';
+  @Input() public state: 'hidden' | 'revealed' | 'removed' = 'hidden';
 
   constructor() { }
 
